59ed974610be6b328f560f42ca9b244aed8f00c0
scienceopen <scienceopen@users.noreply.github.com>
2015-07-23 23:32:54 -0400

    efficiency

--- angledist.m
+++ angledist.m
@@ -5,10 +5,11 @@ function angdistdeg = angledist(az1,el1,az2,el2)
 % A demonstration of calling Python functions from Matlab R2014b or newer
 % Michael Hirsch
 
-% add the path to the https://github.com/scienceopen/pymap3d package
-P = py.sys.path;
-P.append('../pymap3d')
-
-angdistdeg = py.haversine.angledist(az1,el1,az2,el2);
-
+try
+    angdistdeg = py.haversine.angledist(az1,el1,az2,el2);
+catch
+    % add the path to the https://github.com/scienceopen/pymap3d package
+    P = py.sys.path;
+    P.append('../pymap3d')
+    angdistdeg = py.haversine.angledist(az1,el1,az2,el2);
 end
