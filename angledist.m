3394d66fcbf165bc815a6fbf4e596ccecd31f78b
scivision <scivision@users.noreply.github.com>
2015-08-25 12:29:56 -0400

    doc

--- angledist.m
+++ angledist.m
@@ -3,6 +3,9 @@ function angdistdeg = angledist(az1,el1,az2,el2)
 % computes angular distance for a pair of points given the azimuth, elevation of each
 % point. 
 % A demonstration of calling Python functions from Matlab R2014b or newer
+%
+% Assumes https://github.com/scienceopen/pymap3d is in adjacent directory ../pymap3d
+%
 % Michael Hirsch
 
 try
