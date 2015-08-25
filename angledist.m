ef826218ca2928b493b4471752c93fb29e117650
scienceopen <scienceopen@users.noreply.github.com>
2015-08-25 12:40:50 -0400

    old version trap

--- angledist.m
+++ angledist.m
@@ -11,6 +11,10 @@ function angdistdeg = angledist(az1,el1,az2,el2)
 try
     angdistdeg = py.haversine.angledist(az1,el1,az2,el2);
 catch
+    if verLessThan('matlab','8.4')
+        error('Python is not in Matlab older than R2014b')
+    end
+    
     % add the path to the https://github.com/scienceopen/pymap3d package
     P = py.sys.path;
     P.append('../pymap3d')
