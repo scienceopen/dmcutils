b75cb2c72633bd52568c1c14b92d0db581db1132
scienceopen <scienceopen@users.noreply.github.com>
2015-09-25 13:29:18 -0400

    move note

--- andor_neo_spool/flatdark.m
+++ andor_neo_spool/flatdark.m
@@ -1,14 +1,16 @@
-function [flatnorm,dark] = flatdark(flatfn,darkfn)
+% not correct program
+
+function [flatnorm,dark] = flatdark(flatfn,darkfn,imsize)
 %% dark
 fid = fopen(darkfn,'rb');
-darkraw = fread(fid,imsize,'uint16');
+darkraw = fread(fid,imsize,'uint16=>uint16');
 fclose(fid);
-dark = mean(darkraw(:));
+dark = mean(darkraw(:),'native'); %TODO do we agree with this definition
 %% flat
 fid = fopen(flatfn,'rb');
-flatraw = fread(fid,imsize,'uint16');
+flatraw = fread(fid,imsize,'uint16=>uint16');
 fclose(fid);
 
-flat = flatraw - dark;
-flatnorm = flat / max(flat(:));
+flat = flatraw - dark; %uint16 clips to 0, different behavior than Python/Numpy !
+flatnorm = double(flat) / double(max(flat(:))); %TODO this is probably wrong
 end
