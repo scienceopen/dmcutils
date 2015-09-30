0093d5bb51d0b500104d775e8fde836b937e9189
Michael H <hirsch617@gmail.com>
2015-09-30 08:35:37 -0400

    test

--- ConvertSpool2h5.py
+++ ConvertSpool2h5.py
@@ -4,17 +4,52 @@ Michael Hirsch
 Sept 2015
 """
 from __future__ import division,absolute_import
+import h5py
+from os.path import expanduser,join
+from numpy import arange,asarray,empty,int16
+from glob import glob
+#
 from histutils.rawDMCreader import dmcconvert,doPlayMovie,doplotsave
-from histutils.timedmc import frame2ut1,ut12frame
+from histutils.timedmc import frame2ut1
 
-def goRead(fn,xy,bn,kineticsec,startutc):
+datatype=int16
 
+def h5toh5(fn,kineticsec,startutc):
+    with h5py.File(expanduser(fn),'r',libver='latest') as f:
+        data = f['/rawimg'].value
+
+    rawind = arange(data.shape[0])+1
+
+    ut1 = frame2ut1(startutc,kineticsec,rawind)
+
+    return data,rawind,ut1
+
+def oldspool(path,xy,bn,kineticsec,startutc):
+    import matlab.engine
+    eng = matlab.engine.start_matlab()
+
+    path = expanduser(path)
+    flist = glob(join(path,'*.dat'))
+    nfile = len(flist)
+    print('Found {} .dat files in {}'.format(nfile,path))
+
+    nx,ny= xy[0]//bn[0], xy[1]//bn[1]
+    data = empty((nfile,ny,nx),dtype=datatype) #NOTE: Assumes one frame per file!
+    for i,f in enumerate(flist):
+       print('processing {}   {} / {}'.format(f,i+1,nfile))
+       datmat = eng.readNeoPacked12bit(f, nx,ny)
+       assert datmat.size == (ny,nx)
+       data[i,...] = asarray(datmat,dtype=datatype)
+
+    eng.quit()
+
+    return data,None,None
 
 if __name__ == "__main__":
     from argparse import ArgumentParser
     p = ArgumentParser(description='Andor Neo Spool reader, plotter, converter')
-    p.add_argument('infile',help='.DMCdata file name and path')
-    p.add_argument('-p','--pix',help='nx ny  number of x and y pixels respectively',nargs=2,default=(512,512),type=int)
+    p.add_argument('path',help='path containing 12-bit Neo spool files in broken format (2008-spring 2011)')
+    p.add_argument('-p','--pix',help='nx ny  number of x and y pixels respectively',nargs=2,default=(2544,2160),type=int)
     p.add_argument('-b','--bin',help='nx ny  number of x and y binning respectively',nargs=2,default=(1,1),type=int)
     p.add_argument('-m','--movie',help='seconds per frame. ',type=float)
     p.add_argument('-c','--clim',help='min max   values of intensity expected (for contrast scaling)',nargs=2,type=float)
@@ -34,14 +69,14 @@ if __name__ == "__main__":
     params = {'kineticsec':p.kineticsec,'rotccw':p.rotccw,'transpose':p.transpose,
               'flipud':p.flipud,'fliplr':p.fliplr,'fire':p.fire}
 
-    rawImgData,rawind,finf,ut1_unix = goRead(p.infile, p.pix,p.bin,p.kineticsec,p.startutc)
+    rawImgData,rawind,ut1_unix = h5toh5(p.path,p.kineticsec,p.startutc)
 #%% convert
-    dmcconvert(finf,p.infile,rawImgData,ut1_unix,rawind,p.output,params)
+    dmcconvert(rawImgData,ut1_unix,rawind,p.output,params)
 #%% plots and save
     try:
         from matplotlib.pyplot import show
         doPlayMovie(rawImgData,p.movie, ut1_unix=ut1_unix,clim=p.clim)
-        doplotsave(p.infile,rawImgData,rawind,p.clim,p.hist,p.avg)
+        doplotsave(p.path,rawImgData,rawind,p.clim,p.hist,p.avg)
         show()
     except Exception as e:
         print('skipped plotting  {}'.format(e))
--- readNeoPacked12bit.m
+++ readNeoPacked12bit.m
@@ -1,36 +1,38 @@
-function data = readNeoPacked12bit(datafile,imsize)
+function data = readNeoPacked12bit(datafile,nx,ny)
 %% Reading of "damaged" 12-bit packed Neo files from 2008 beta version of Andor Solis
 % endian-flipping by Bob Marshall, Stanford U.
 % requires Matlab -- does not work in GNU Octave 4.0.
+%
+% NOTE: assumes one frame per file
+%
 % Michael Hirsch
 
 fs = dir(datafile);
 fs = fs.bytes;
 
-bytesperframe = prod(imsize)*12/8;
+bytesperframe = nx*ny*12/8;
 nframe = fix(fs/bytesperframe);
+assert(nframe==1,'see comments to allow more than 1 frame per file')
 
 f = fopen(datafile,'rb');
-data = zeros([imsize,nframe],'uint16');
+%data = zeros([nx,ny,nframe],'uint16');
+data = zeros([nx,ny],'uint16');
 
-
-for i = 1:nframe 
-cf = ftell(f);
-%%read big-endian odd rows
-dataBE = fread(f,imsize,'ubit12=>uint16','ieee-be'); % *ubit12 is equivalent
-fseek(f,cf,'bof'); %go back so we can read it again (inefficient)
-% read little-endian even rows
-dataLE = fread(f,imsize,'ubit12=>uint16','ieee-le'); 
+%for i = 1:nframe 
+    cf = ftell(f);
+%% read big-endian odd rows
+    dataBE = fread(f,[nx,ny],'ubit12=>uint16','ieee-be'); % *ubit12 is equivalent
+    fseek(f,cf,'bof'); %go back so we can read it again (inefficient)
+%% read little-endian even row
+    dataLE = fread(f,[nx,ny],'ubit12=>uint16','ieee-le'); 
 %% combine the appropriate rows
-data(1:2:end,:,i) = dataBE(1:2:end,:);
-data(2:2:end,:,i) = dataLE(2:2:end,:);
+    data(1:2:end,:) = dataBE(1:2:end,:);
+    data(2:2:end,:) = dataLE(2:2:end,:);
 
-end %for
+%end %for
 
 fclose(f);
+
+data = data.';
 end
 
-%% 2x2 binning (untested)
-%data2 = 0.25 * ( data(1:2:end,1:2:end) + data(1:2:end,2:2:end) ...
-%    + data(2:2:end,1:2:end) + data(2:2:end,2:2:end) );
-%data2 = data;
