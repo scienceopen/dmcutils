d790646ca55e0016256aae8efde220abfd9a469e
Michael Hirsch <scienceopen@users.noreply.github.com>
2015-10-14 23:15:41 -0400

    Update expanduser.m

--- expanduser.m
+++ expanduser.m
@@ -1,5 +1,10 @@
 function expanded = expanduser(p)
-
+%
+% For now, handles only a leading tilde, does not currently handle specifying ~otheruser
+% example:
+% expanduser('~/Downloads/foo')
+% ans = /home/joespc/Downloads/foo
+%
 if ischar(p) && size(p,1) == 1
     if strcmp(p(1),'~')
         expanded = [getenv('HOME'),p(2:end)];
