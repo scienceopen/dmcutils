232b5508e752d2908ba8dd2b452f4f455d3b62a9
Michael H <hirsch617@gmail.com>
2015-10-20 16:16:40 -0400

    make work with 2015 spool files

--- NeoSpoolPlayer.m
+++ NeoSpoolPlayer.m
@@ -1,22 +1,28 @@
-function NeoSpoolPlayer(fn)
-
+function NeoSpoolPlayer(spoolfn,nx,ny)
+showhist = false;
 nBin = 128; %histogram bins
-
-[data tick64 nFrame] = NeoSpoolReader(fn);
-
-figure(1)
-h.img = imagesc(data(:,:,1));
-h.t = title(['FPGA tick #',int2str(tick64(1))]);
+%% get data
+[data, tick64] = readNeoSpool(spoolfn,nx,ny);
+nFrame = size(data,3);
+%% histogram
+if showhist
+    figure(2),clf(2)
+    hist(single(data(:)),128),set(gca,'yscale','log')
+    title(['Histogram of ',spoolfn,' ',int2str(nBin),' bins'])
+    xlabel('data number'), ylabel('occurences')
+end
+%% video
+figure(2),clf(2)
+hi=imagesc(data(:,:,1));
+ht=title(['FPGA tick #',int2str(tick64(1))]);
 colormap('gray')
+clim = prctile(single(data(:)),[1,99.9]); % single() needed for Matlab R2015b et al
+set(gca,'clim',clim)
+colorbar
 
-figure(2)
-hist(single(data(:)),128),set(gca,'yscale','log')
-title(['Histogram of ',fn,' ',int2str(nBin),' bins'])
-xlabel('data number'), ylabel('occurences')
-
-for i = 2:nFrame
-    set(h.img,'cdata',data(:,:,i))
-    set(h.t,'string',['FPGA tick #',int2str(tick64(i))])
+for i = 1:nFrame
+    set(hi,'cdata',data(:,:,i))
+    set(ht,'string',['FPGA tick #',int2str(tick64(i))])
     pause(0.25)
 end
 
---
+++ readNeoSpool.m
@@ -0,0 +1,45 @@
+% reader of Neo spool files, 16 bit only (not 12-bit pack)
+function [data, tick64] = readNeoSpool(spoolfn,nx,ny,varargin)
+
+p = inputParser;
+addOptional(p,'nFrame',12) % for 2012-2013 Solis, was 11 ...
+addOptional(p,'AOIstride',8) %always 8?
+addOptional(p,'colfirst',true) %false for 2012-2013 Solis?, true for 2015 Solis
+parse(p,varargin{:})
+U = p.Results;
+
+finf = dir(spoolfn);
+
+
+if U.colfirst
+    freadshape = [nx+U.AOIstride, ny];
+else
+    freadshape = [ny+U.AOIstride,nx];
+end
+
+data = zeros(ny,nx, U.nFrame,'uint16'); %preallocate
+tick64 = zeros(U.nFrame,1,'uint64');
+
+fid = fopen(spoolfn);
+for i = 1:U.nFrame
+
+    frame = fread(fid,freadshape,'uint16=>uint16',0,'l');
+    assert(all(size(frame)==freadshape),'wrong image size, did you read past end of data file (wrong nx,ny)?')
+    
+    if U.colfirst
+        data(:,:,i) = transpose(frame(1:end-U.AOIstride,:));
+    else
+        data(:,:,i) = frame(1:end-U.AOIstride,:);
+    end
+    
+    %get FPGA tick index
+    tick64(i) = parseNeoHeader(fid,freadshape(1));
+end %for i
+
+
+assert(ftell(fid)==finf.bytes,'did you read the whole file?')
+assert(all(diff(tick64)>0),'are frames out of order?')
+
+fclose(fid);
+
+end %function
