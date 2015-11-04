7cf204a56867d6d0da9d46d616644c6380cbd1b4
Michael H <hirsch617@gmail.com>
2015-11-04 09:54:36 +0000

    checks

--- preview.sh
+++ preview.sh
@@ -5,11 +5,14 @@ nx=$2
 ny=$3
 [[ -d $root ]] || { echo "$root not a directory"; exit 1; }
 
-python Webserver.py 8088
+#todo detect if server already running
+python Webserver.py 8088 2>>$HOME/server.log &
+ret=$?
+[[ $ret -ne 0 ]] && { echo "server already running?"; exit $ret; }
 
-for i in {0..360}; do
-    pythonwin live_preview_neospool.py $root $nx $ny
+while :; do
+    pythonwin live_preview_neospool.py $root $nx $ny 2>>$HOME/live.log
     ret=$?
     [[ $ret -ne 0 ]] && { echo "python program error"; exit $ret; }
-    sleep 60
+    sleep 600
 done
