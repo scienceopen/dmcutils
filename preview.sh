a4cafd557647c71610d9894710413d5a8dd267d3
michael <scienceopen@users.noreply.github.com>
2015-11-04 04:35:16 -0500

    api

--- preview.sh
+++ preview.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+root=$1
+nx=$2
+ny=$3
+[[ -d $root ]] || { echo "$root not a directory"; exit 1; }
+
+
+
 for i in {0..360}; do
-    pythonwin live_preview_neospool.py e:\2015-10-31 400 400
+    pythonwin live_preview_neospool.py $root $nx $ny
+    ret=$?
+    [[ $ret -ne 0 ]] && { echo "python program error"; exit $ret; }
     sleep 60
 done
