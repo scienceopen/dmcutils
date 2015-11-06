2978bb7eb411119b0b841b6b2a5c43e5bca76cec
Michael H <scienceopen@users.noreply.github.com>
2015-11-06 01:56:58 -0500

    api update, notes

--- preview.sh
+++ preview.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+# simple web server with polling of latest image for Andor Neo Spool Files
+# Michael Hirsch
+#
+# Prereqs: flask flask-limiter pathlib2
+
 
 root=$1
-nx=$2
-ny=$3
 [[ -d $root ]] || { echo "$root not a directory"; exit 1; }
-[[ -z $ny ]] && { echo "need to enter x and y pixel shape"; exit 1; }
 ##### detect if server already running, if not, start it
 nice -n 15 python Webserver.py 8088 2>>$HOME/server.log &
 ret=$?
@@ -17,8 +19,7 @@ esac
 #### every N seconds (600=10 minutes) update the preview.
 # Note because of non-sequential file naming, this takes a few minutes each time, 
 # proportional to the rapidly increasing number of spool files...
-# maybe parsing .sifx would help immediately find the most recent file?
 while :; do
-    nice -n 19 $pyloop -u live_preview_neospool.py $root $nx $ny 2>>$HOME/live.log
+    nice -n 19 $pyloop -u live_preview_neospool.py $root 2>>$HOME/live.log
     sleep 600
 done
