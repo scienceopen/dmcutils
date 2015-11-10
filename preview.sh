254305ca8bd7a3431bda38afa9830e670d8bd636
Michael Hirsch <scienceopen@users.noreply.github.com>
2015-11-10 00:39:45 -0500

    Update preview.sh

--- preview.sh
+++ preview.sh
@@ -9,7 +9,7 @@
 root=$1
 [[ -d $root ]] || { echo "$root not a directory"; exit 1; }
 ##### detect if server already running, if not, start it
-nice -n 15 python Webserver.py 8088 2>>$HOME/server.log &
+nice -n 15 python3 Webserver.py 8088 2>>$HOME/server.log &
 ret=$?
 [[ $ret -ne 0 ]] && { echo "server already running?"; }
 ##### we use Windows Anaconda on Windows, arbitrary choice.
