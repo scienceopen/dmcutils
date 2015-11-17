0d9c2ed44227673d0e2760cd784fd24985e0cb37
michael <scienceopen@users.noreply.github.com>
2015-11-17 11:00:54 -0500

    use astropy

--- angledist.m
+++ angledist.m
@@ -1,22 +1,25 @@
 function angdistdeg = angledist(az1,el1,az2,el2)
 %%
 % computes angular distance for a pair of points given the azimuth, elevation of each
-% point. 
+% point.  
 % A demonstration of calling Python functions from Matlab R2014b or newer
 %
-% Assumes https://github.com/scienceopen/pymap3d is in adjacent directory ../pymap3d
+% input and output are in DEGREES
 %
 % Michael Hirsch
 
 try
-    angdistdeg = py.haversine.angledist(az1,el1,az2,el2);
-catch
+    ud = py.astropy.units.deg;
+    angdist = py.astropy.coordinates.angle_utilities.angular_separation(az1*ud,el1*ud,az2*ud,el2*ud);
+    angdistdeg = angdist.to(ud).value;
+catch e
     if verLessThan('matlab','8.4')
-        error('Python is not in Matlab older than R2014b')
-    end
-    
-    % add the path to the https://github.com/scienceopen/pymap3d package
-    P = py.sys.path;
-    P.append('../pymap3d')
-    angdistdeg = py.haversine.angledist(az1,el1,az2,el2);
+        error('Python is not available in Matlab older than R2014b')
+    elseif strcmp(e.identifier,'MATLAB:undefinedVarOrClass')
+        disp('do you have AstroPy installed in your Python path?')
+        disp(py.sys.path)
+        rethrow(e)
+    else
+        rethrow(e)
+    end   
 end
