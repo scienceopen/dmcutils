134493a5d8b4bbd0f328247db9dc4c25d8421c87
scienceopen <scienceopen@users.noreply.github.com>
2016-01-14 13:14:06 -0500

    doc

--- isoctave.m
+++ isoctave.m
@@ -1,6 +1,6 @@
 function oct = isoctave()
-%Michael Hirsch Oct 2012
-% tested with Octave 3.6.3/4.0.0 and Matlab R2012a/R2015a
+%Michael Hirsch
+% tested with Octave 3.6-4.0 and Matlab R2012a-R2016a
 
 oct = exist('OCTAVE_VERSION', 'builtin') == 5;
 
