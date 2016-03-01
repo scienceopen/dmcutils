84e895dfdeea836840ed033e9269b9a0d11c7976
scienceopen <scienceopen@users.noreply.github.com>
2016-03-01 01:26:41 -0500

    move

--- setup.py
+++ setup.py
@@ -21,7 +21,6 @@ setup(name='dmcutils',
 
 #%%
 try:
-    subprocess.call(['conda','install','--yes','--quiet','--file','requirements.txt'],shell=False) #don't use os.environ
+    subprocess.run(['conda','install','--yes','--quiet','--file','requirements.txt'])
 except Exception as e:
-    print('you will need to install packages in requirements.txt  {}'.format(e))
-
+    pass
