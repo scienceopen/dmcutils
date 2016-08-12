557abbc2347ff7ffe5c764ae727e999767b995ff
michael <scienceopen@users.noreply.github.com>
2016-08-12 11:29:33 -0400

    write fits header to hdf5 for ref.

--- ConvertSolisFits2h5.py
+++ ConvertSolisFits2h5.py
@@ -3,20 +3,19 @@
 converts multiple Andor Solis FITS files into one HDF5 with timestamps
 watch RAM consumption
 """
-
-from tempfile import mkstemp
 from numpy import nan
 #
 from dmcutils.fitsreadermulti import fitsreadermulti
-from histutils.rawDMCreader import dmcconvert
+from histutils.vid2h5 import vid2h5
 
 def main(flist,ofn, P,cmdlog):
 
-    ut1_unix,rawind,kineticsec = fitsreadermulti(flist,ofn)
+    ut1_unix,rawind,kineticsec,header = fitsreadermulti(flist,ofn)
 
     P['kineticsec'] = kineticsec
+    P['header'] = header
 
-    dmcconvert(None,ut1_unix, rawind, ofn, P, cmdlog)
+    vid2h5(None,ut1_unix, rawind, ofn, P, cmdlog)
 
 
 
@@ -25,7 +24,7 @@ if __name__ == '__main__':
     from argparse import ArgumentParser
     p = ArgumentParser(description='converts multiple Andor Solis FITS files into one HDF5 with timestamps')
     p.add_argument('flist',help='file(s) to convert to one HDF5 file',nargs='+')
-    p.add_argument('-o','--ofn',help='extract raw data into this file [h5]',default=mkstemp('.h5')[1])
+    p.add_argument('-o','--ofn',help='extract raw data into this file [h5]',default='.')
     p.add_argument('--rotccw',help='rotate CCW value in 90 deg. steps',type=int,default=0)
     p.add_argument('--transpose',help='transpose image',action='store_true')
     p.add_argument('--flipud',help='vertical flip',action='store_true')
