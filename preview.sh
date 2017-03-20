0926541e636ae2e3693ef108a00d0cbdbd1b09de
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-03-20 01:50:43 -0400

    early python path detect

--- preview.sh
+++ preview.sh
@@ -9,15 +9,18 @@
 
 root=$1
 [[ -d $root ]] || { echo "$root not a directory"; exit 1; }
+
+case "$(uname -s)" in
+    CYGWIN*) root=$(cygpath --windows $root); pyloop=/cygdrive/c/Miniconda3/python; ;;
+    *) pyloop=python3 ;;
+esac
+
 ##### detect if server already running, if not, start it
 nice -n 15 python3 Webserver.py 8088 2>>$HOME/server.log &
 ret=$?
 [[ $ret -ne 0 ]] && { echo "server already running?"; }
 ##### we use Windows Anaconda on Windows, arbitrary choice.
-case "$(uname -s)" in
-    CYGWIN*) root=$(cygpath --windows $root); pyloop=/cygdrive/c/Miniconda3/python; ;;
-    *) pyloop=python3 ;;
-esac
+
 #### every N seconds (600=10 minutes) update the preview.
 # Note because of non-sequential file naming, this takes a few minutes each time, 
 # proportional to the rapidly increasing number of spool files...
