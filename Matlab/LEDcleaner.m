bd95ff746ae43fea057df3c75cc9d56217e3fe4d
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-05-24 15:48:15 -0400

    doc [ci skip]

--- Matlab/LEDcleaner.m
+++ Matlab/LEDcleaner.m
@@ -1,7 +1,7 @@
 function data = LEDcleaner(varargin)
 %% LED reflection cleanser
 % requires Octave 4.0+ or Matlab
-% assumes 'histutils' is in adjacent directory https://github.com/scienceopen/histutils
+% assumes 'histutils' is in adjacent directory https://github.com/scivision/histutils
 %
 % A program that takes out the flashing LED reflection from the Jan 13,
 % 2013 DMC CCD data. A nice mutual observation between CMOS and CCD, but CCD
