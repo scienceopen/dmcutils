887a0e6720a31a6a0467543902ec41b20fdee112
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-01-17 18:26:55 -0500

    template

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
-req=['nose','python-dateutil','pytz','pandas','h5py','scikit-image',
+install_requires=['python-dateutil','pytz','pandas','h5py','scikit-image',
      'histutils','morecvutils']
+tests_require=['nose','coveralls']
 # %%
 from setuptools import setup,find_packages
 
@@ -10,10 +11,12 @@ setup(name='dmcutils',
       url='https://github.com/scivision/dmcutils',
       description='Utilities to read and plot DMC Experiment data',
       version='0.9',
-	  install_requires=req,
+	  install_requires=install_requires,
+	  tests_require=tests_require,
 	  python_requires='>=3.6',
       extras_require={'web':['flask','flask-limiter'],
                       'plot':['matplotlib'],
-                      'io':['astrometry_azel'],},
+                      'io':['astrometry_azel'],
+                      'tests':tests_require},
 	  )
 
